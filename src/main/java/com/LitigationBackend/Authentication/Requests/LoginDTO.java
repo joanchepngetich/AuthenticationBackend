e03fe21463cbc2c05d1ef6a7522e9776771046b3
joanchepngetich <joanchepngetich01@gmail.com>
2024-05-06 10:27:42 +0300

    LoginDTO added

---
+++ src/main/java/com/LitigationBackend/Authentication/Requests/LoginDTO.java
@@ -0,0 +1,38 @@
+package com.LitigationBackend.Authentication.Requests;
+
+public class LoginDTO {
+    private String email;
+    private String password;
+
+    public LoginDTO() {
+    }
+
+    public LoginDTO(String email, String password) {
+        this.email = email;
+        this.password = password;
+    } //create getters and setters
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "LoginDto{" +
+                "email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
+
